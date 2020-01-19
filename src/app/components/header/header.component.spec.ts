e10eb6a474748510ac4a370065535320b57dde7e
Kristiyan Todorov <kristiyantkirchev@gmail.com>
2020-01-19 16:46:56 +0200

    add mobile menu

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,11 +1,10 @@
-import { render, fireEvent } from "@testing-library/angular";
+import { render } from "@testing-library/angular";
 import { HeaderComponent } from "./header.component";
 import { routes } from "src/app/app-routing.module";
 import { LaunchesContainerComponent } from "../launches-container/launches-container.component";
 import { AppComponent } from "src/app/app.component";
 import { LaunchCardComponent } from "../launch-card/launch-card.component";
 import { HomeComponent } from "../home/home.component";
-import { Router } from "@angular/router";
 
 describe("HeaderComponent", () => {
   it("should render", async () => {
@@ -21,5 +20,29 @@ describe("HeaderComponent", () => {
     });
     const launchesLink = getByText("Launches");
     click(launchesLink);
+  });
+  it("should render", async () => {
+    const { getByTestId, click } = await render(HeaderComponent, {
+      declarations: [
+        AppComponent,
+        LaunchesContainerComponent,
+        LaunchCardComponent,
+        HeaderComponent,
+        HomeComponent
+      ],
+      routes
+    });
+    const mobileMenuButton = getByTestId("mobile-menu-button");
+    const mobileMenu = getByTestId("mobile-menu");
+
+    expect(mobileMenuButton.classList).not.toContain("is-active");
+    expect(mobileMenu.classList).not.toContain("is-active");
+    click(mobileMenuButton);
+
+    const activeMenuButton = getByTestId("mobile-menu-button");
+    const activeMobileMenu = getByTestId("mobile-menu-button");
+
+    expect(activeMenuButton.classList).toContain("is-active");
+    expect(activeMobileMenu.classList).toContain("is-active");
   });
 });
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,13 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html"
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isActive: boolean = false;
+  toggleIsActive() {
+    console.log(this.isActive);
+    this.isActive = !this.isActive;
+  }
+}
